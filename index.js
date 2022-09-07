1ff2710596395f59484b57f99aa10b14905f5f86
valerieyang00 <valerieyang00@gmail.com>
2022-09-07 14:33:02 -0700

    added some more helpful comments

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const app = express()
 const PORT = 3001
 
 app.set("view engine", "ejs")
+// app.use == middleware function, next() at end of any use function will move the express to next middleware
 app.use(layout)
 // tell express to listen for request bodies sent from HTML forms
 app.use(express.urlencoded({ extended: false}))
+//For PUT and DELETE INSTALL method override first then enable here
 const methodOverride = require("method-override")
 app.use(methodOverride("_method"))
 
